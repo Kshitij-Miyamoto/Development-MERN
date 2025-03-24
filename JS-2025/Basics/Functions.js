a95930c43a7d9328a3c29481eeceb02330d327b5
Kshitij Sharma <110044782+Kshitij210@users.noreply.github.com>
2025-03-24 17:21:48 +0530

    New commits

--- JS-2025/Basics/Functions.js
+++ JS-2025/Basics/Functions.js
@@ -5,3 +5,18 @@ result = greet("salman") //Here Salman is arguements
 console.log(greet("kshitij"))  //Declared in global scope
 console.log(result);
 
+function CalculateCartPrice(val1 , val2, ...num1){
+  return num1 
+}
+console.log(CalculateCartPrice(2));
+console.log(CalculateCartPrice(200 , 300, 500 , 400)); //Now we need to pass more than one values, we use REST operstor ...num1
+
+const user = {
+       username : "Kshitij" , 
+       price : 200
+
+}
+
+function handledObject(anyobject){
+    
+}
